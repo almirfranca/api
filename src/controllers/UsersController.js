142e7b41fab00e3996f94b77b9e644db070d8938
almirfranca <almirfrancajob@gmail.com>
2024-05-01 12:46:53 -0300

    I separated some responsibilities/ and put it back-end online

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,9 @@
 const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
+
+const UserRepository = require("../repositories/UserRepository");
 const sqliteConnection = require("../database/sqlite");
+const UserCreateService = require("../services/UserCreateService");
 
 class UsersController {
   /**
@@ -13,58 +16,58 @@ class UsersController {
 
   //create
   async create(request, response) {
-    const { name, email, password} = request.body;
+    const { name, email, password } = request.body;
 
-    const database = await sqliteConnection();
-    const checkUserExists = await database.get("SELECT * FROM  users WHERE email = (?)", [email]);
-  
-    if(checkUserExists) {
-      throw new AppError('Esse usuário já está em uso.');
-    }
-
-    const hashedPassword = await hash(password, 8);
-
-    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]);
+    const userRepository = new UserRepository();
+    const userCreateService = new UserCreateService(userRepository);
+    await userCreateService.execute({ name, email, password });
 
     return response.status(201).json();
   }
 
   async update(request, response) {
-    const {name, email, password, old_password} = request.body;
+    const { name, email, password, old_password } = request.body;
     const user_id = request.user.id;
 
     const database = await sqliteConnection();
-    const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id]);
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [
+      user_id,
+    ]);
 
-    if(!user) {
-      throw new AppError("Usuário não encontrado.")
+    if (!user) {
+      throw new AppError("Usuário não encontrado.");
     }
 
-    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+    const userWithUpdatedEmail = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
 
-    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
-      throw new AppError("Este e-mail já está em uso.")
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.");
     }
 
     user.name = name ?? user.name;
     user.email = email ?? user.email;
 
-    if(password && !old_password) {
-      throw new AppError("Você precisa infornar a senha antiga para definir a nova senhar.");
+    if (password && !old_password) {
+      throw new AppError(
+        "Você precisa infornar a senha antiga para definir a nova senhar."
+      );
     }
 
-    if(password && old_password) {
+    if (password && old_password) {
       const checkOldPassword = await compare(old_password, user.password);
 
-      if(!checkOldPassword) {
+      if (!checkOldPassword) {
         throw new AppError("A senha antiga não confere.");
       }
 
       user.password = await hash(password, 8);
     }
 
-
-    await database.run(`
+    await database.run(
+      `
       UPDATE users SET
       name = ?,
       email = ?,
